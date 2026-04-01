b607c19c405f684bfdc92431c08d0f0b3648e8dd
whypikmi <ttelatnikova1@yandex.ru>
2026-04-01 08:39:04 +0300

    Initial commit

---
+++ app/src/main/java/com/shcherbakov/student_planner_androidstudio_15/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.shcherbakov.student_planner_androidstudio_15
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shcherbakov.student_planner_androidstudio_15.ui.theme.Student_Planner_androidstudio_15Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Student_Planner_androidstudio_15Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Student_Planner_androidstudio_15Theme {
+        Greeting("Android")
+    }
+}
