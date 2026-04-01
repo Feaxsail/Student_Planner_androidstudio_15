112f4648b33b7b7419ce22a1394bf2552b71c50e
whypikmi <ttelatnikova1@yandex.ru>
2026-04-01 08:51:23 +0300

    Step 2: Create Screen sealed class for routes

---
+++ app/src/main/java/com/shcherbakov/student_planner_androidstudio_15/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.shcherbakov.student_planner_androidstudio_15.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Details : Screen("details/{subjectId}") {
+        fun createRoute(subjectId: String) = "details/$subjectId"
+    }
+    object Profile : Screen("profile")
+    object Settings : Screen("settings")
+}
